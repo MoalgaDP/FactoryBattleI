34db3e1b5aae3709aa68c3b7346e5ae77de95bf2
s1500729 <dicenull.dev@gmail.com>
2022-05-03 21:07:18 +0900

    ジャンプでアイテムを追加するようにした

--- Assets/Scripts/Player/InventoryController.cs
+++ Assets/Scripts/Player/InventoryController.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    [SerializeField] List<Item> Inventory;
+    [SerializeField] ReactiveCollection<Item> inventory;
 
+    public ReactiveCollection<Item> Inventory { get { return inventory; } }
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            inventory.Add(new Item());
+        }
+    }
 }
