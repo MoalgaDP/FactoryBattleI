bb2f03239811450312596207202dea81b898ac77
s1500729 <dicenull.dev@gmail.com>
2022-05-04 21:08:43 +0900

    フォーカスが無駄に生成されていたため修正

--- Assets/Scripts/ui/InventoryViewer.cs
+++ Assets/Scripts/ui/InventoryViewer.cs
@@ -11,8 +11,10 @@ public class InventoryViewer : MonoBehaviour
 {
     [SerializeField] GameObject itemPrefab;
 
+
     [SerializeField] GameObject focusPrefab;
     GameObject focusObj;
+
 
     InventoryController inventoryController;
 
@@ -20,43 +22,55 @@ public class InventoryViewer : MonoBehaviour
 
     void Start()
     {
+
         var player = GameObject.FindWithTag("Player");
         inventoryController = player.GetComponent<InventoryController>();
 
         var inventory = inventoryController.Inventory;
         inventory.ObserveCountChanged(notifyCurrentCount: true).Subscribe(_ =>
         {
+            if (focusObj != null)
+                Destroy(focusObj.gameObject);
             foreach (Transform child in transform)
             {
                 Destroy(child.gameObject);
             }
 
+            focusObj = Instantiate(focusPrefab, Vector3.zero, Quaternion.identity);
             foreach (var (container, index) in inventory.Select((v, i) => (v, i)))
             {
                 var itemObj = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity, this.transform);
                 itemObj.name = index.ToString();
+
+                if (selectIndex == index)
+                {
+                    changeFocus(itemObj.transform);
+                }
             }
 
-            focusObj = Instantiate(focusPrefab, Vector3.zero, Quaternion.identity);
-            focusObj.SetActive(false);
+
         });
 
         this.ObserveEveryValueChanged(viewer => viewer.selectIndex).Subscribe(index =>
         {
             if (index < 0)
             {
-                focusObj.SetActive(false);
+                focusObj.GetComponent<Image>().enabled = false;
                 return;
             }
             inventoryController.HoldingContainer = inventory[index];
             var itemObj = transform.GetChild(index);
-
-
-            focusObj.SetActive(true);
-            focusObj.transform.SetParent(itemObj);
-            (focusObj.transform as RectTransform).anchoredPosition = Vector3.zero;
+            changeFocus(itemObj);
         });
 
+    }
+
+    void changeFocus(Transform itemObj)
+    {
+
+        focusObj.GetComponent<Image>().enabled = true;
+        focusObj.transform.SetParent(itemObj);
+        (focusObj.transform as RectTransform).anchoredPosition = Vector3.zero;
     }
 
 }
